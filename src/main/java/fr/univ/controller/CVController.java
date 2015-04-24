524162de94ad63aade3dc504b7497097feb7d256
Valulz <vdesportes@gmail.com>
2015-04-24 10:10:49 +0200

    Add MongoDB connection & new URL : /rest/

--- src/main/java/fr/univ/controller/CVController.java
+++ src/main/java/fr/univ/controller/CVController.java
@@ -5,7 +5,10 @@ import fr.univ.modele.CVList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/resume")
@@ -27,7 +30,9 @@ public class CVController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public @ResponseBody CVList getCVInXML(){
+    public @ResponseBody CVList getCVInXML() throws UnknownHostException {
+        DBCollection coll = getDatabase();
+
         return list;
     }
 
@@ -38,5 +43,34 @@ public class CVController {
 
         return cvEntry;
     }
+
+    /**
+     * Connect to the MongoDB database for the cv
+     * @return the collection of cv
+     * @throws UnknownHostException if the database can't be found
+     */
+    private DBCollection getDatabase() throws UnknownHostException {
+        final String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        final String port = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
+        final String user = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+        final String pass = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+        final String dbName = "lw2";
+        final String collection = "cv";
+
+        MongoCredential credential = MongoCredential.createCredential(user, dbName, pass.toCharArray());
+
+        MongoClient mongoClient = new MongoClient(
+                new ServerAddress(host, Integer.parseInt(port)),
+                Arrays.asList(credential)
+        );
+
+        DB db = mongoClient.getDB(dbName);
+        db.createCollection(collection, null);
+
+        return db.getCollection(collection);
+    }
+
+
+
 
 }
